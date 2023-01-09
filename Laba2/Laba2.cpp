30ed7207eada606363d45fc017abc55e88cf0431
IwaMiracle <90666670+IwaMiracle@users.noreply.github.com>
2023-01-09 15:55:16 +0700

    Добавьте файлы проекта.

---
+++ Laba2/Laba2.cpp
@@ -0,0 +1,16 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <iostream>
+#include <conio.h>
+using namespace std;
+
+
+class weapon
+{
+
+};
+
+int main()
+{
+    setlocale(LC_ALL, "ru");
+}
